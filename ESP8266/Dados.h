68fb2febe59f022c5a42a2603c4de1680672e558
Calebe Oliveira <30943380+calebeof@users.noreply.github.com>
2019-02-17 14:08:54 -0300

    Adicionando classe Dados

---
+++ ESP8266/Dados.h
@@ -0,0 +1,20 @@
+#ifndef DADOS_H
+#define DADOS_H
+
+class Dados{
+private:
+    String LOCATION, UPLOAD, DOWNLOAD, STATUS, RELE;
+    bool CONEXAO; 
+public:
+    String JSON_SENSOR(double tempC);
+    String JSON_RELE(bool estado);
+    String JSON_STATUS();
+    void uploadSENSOR();
+    void uploadRELE();
+    void uploadSTATUS();
+    void updateRELE();
+    String downloadWEB();
+    bool verifySTATUS();
+};
+
+#endif // DADOS_H
