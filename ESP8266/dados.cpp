1721e1ce56eb5e60bd9697183d703c0da174f0c3
Calebe Oliveira <30943380+calebeof@users.noreply.github.com>
2019-02-17 14:08:37 -0300

    Adicionando classe Dados

---
+++ ESP8266/dados.cpp
@@ -0,0 +1,57 @@
+#include "dados.h"
+
+Dados::Dados(){
+    LOCATION = "http://thermobeer.herokuapp.com/";
+    UPLOAD = "upload";
+    DOWNLOAD = "download";
+    RELE = "\"RELE\":";
+    STATUS =  "\"STATUS\":";
+}
+
+String Dados::JSON_SENSOR(double tempC){
+  return "{" + SENSOR + String(tempC) + "}";
+}
+
+String Dados::JSON_RELE(bool estado){
+  return "{" + RELE + estado + "}";
+}
+
+String Dados::JSON_STATUS(){
+  return "{" + STATUS + CONEXAO + "}";
+}
+
+void Dados::uploadSENSOR(){
+  http.begin(LOCATION+UPLOAD);
+  http.addHeader("Content-Type", "application/json");
+  http.POST(JSON_SENSOR());
+  Serial.print(JSON_SENSOR());
+  http.end();
+} 
+
+void uploadRELE(){
+  http.begin(LOCATION+UPLOAD);
+  http.addHeader("Content-Type", "application/json");
+  http.POST(JSON_RELE());
+  http.end();
+}
+
+void Dados::uploadSTATUS(){
+  http.begin(LOCATION+UPLOAD);
+  http.addHeader("Content-Type", "application/json");
+  http.POST(JSON_STATUS());
+  http.end();
+}
+
+String Dados::downloadWEB(){
+  http.begin(LOCATION+DOWNLOAD);
+  http.GET();
+  String payload = http.getString();
+  http.end();
+  return payload;
+}
+
+bool Dados::verifyStatus(){
+  if(WiFi.status() != WL_CONNECTED)
+    return CONEXAO = false;
+  return CONEXAO = true;  
+}
